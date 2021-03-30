aeded151bb5357088940596336ca014412e5b72f
chittapragada-suchitra <81214703+chittapragada-suchitra@users.noreply.github.com>
2021-03-30 22:36:42 +0530

    Add files via upload

---
+++ BankAccount/BankAccount/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account ac = new Account();
+            Console.WriteLine(ac.accNo);
+            Console.WriteLine(ac.accHolderName);
+            Console.WriteLine(ac.mobile);
+            Console.WriteLine(ac.balance);
+        }
+    }
+}
---
+++ BankAccount/BankAccount/TestTransaction.cs
@@ -0,0 +1,48 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccount
+{
+    class TestTransaction
+    {
+        [TestCase]
+        public void GetAccount()
+        {
+            Account sender = new Account();
+            sender.GetAccount(1212, "John", 8569522226, 10000);
+
+        }
+
+        [TestCase]
+        public void Deposit()
+        {
+            Account sender = new Account();
+            sender.Deposit(2000);
+        }
+        [TestCase]
+        public void WithDraw()
+        {
+            Account sender = new Account();
+            sender.WithDraw(1000);
+
+        }
+
+        [TestCase]
+        public void TransferAmount()
+        {
+            Account sender = new Account();
+            Account receiver = new Account();
+            sender.TransferAmount(receiver, 500);
+
+
+
+            // Assert.AreEqual(500, receiver.balance);
+            //Assert.AreEqual(500, sender.balance);
+
+        }
+    }
+}
